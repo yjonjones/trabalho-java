afa5eb76ebf414cf3c9d2058be625d3170b97b24
yjonez <jonjon.de000@gmail.com>
2024-11-04 16:59:56 -0300

    atualização de codigo

--- src/fil/fil/fitness/FilFilFitness.java
+++ src/fil/fil/fitness/FilFilFitness.java
@@ -3,8 +3,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import banco.ClienteControle;
+import banco.ModaAcademiaControle;
+
 public class FilFilFitness {
-   public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
+
+  ModaAcademiaControle controleACD = new ModaAcademiaControle();
+ do{
+  controleACD.obterTodos().forEach((moda) -> {
+    System.out.println(moda.numRoupa+" :: "+moda.tamanho+
+    " :: "+moda.cor+" :: "+moda.preço+" :: "+moda.quantidadeDisponivel+
+    " :: "+moda.statusEstoque+" :: "+moda.tipoRoupa+" :: "+moda.material);
+  });
+  System.out.println("1-Incluir 2-Excluir 0-Sair");
+  opcao = Integer.parseInt(teclado.readLine());
+  if(opcao==1){
+    ModaAcademia moda = new ModaAcademia();
+    System.out.println("NumRoupa:");
+    moda.numRoupa = Integer.parseInt(teclado.readLine());
+    System.out.println("Tamanho:");
+    moda.tamanho = teclado.readLine();
+    System.out.println("Cor:");
+    moda.cor = teclado.readLine();
+    System.out.println("Preço:");
+    moda.preço = Float.parseFloat(teclado.readLine());
+    System.out.println("Quantidade Disponivel:");
+    moda.quantidadeDisponivel = Integer.parseInt(teclado.readLine());
+    System.out.println("Status Estoque:");
+    moda.statusEstoque = teclado.readLine();
+    System.out.println("Tipo Roupa:");
+    moda.tipoRoupa = teclado.readLine();
+    System.out.println("Material:");
+    moda.material = teclado.readLine();
+    controleACD.incluir(moda);
+  }
+  if(opcao==2){
+    System.out.println("NumRoupa:");
+    controleACD.excluir(Integer.parseInt(teclado.readLine()));
+  }
+ }while(opcao!=0);
  
  ClienteControle controle = new ClienteControle();
  BufferedReader teclado = new BufferedReader(
